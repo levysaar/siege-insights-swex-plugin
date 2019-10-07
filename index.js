c1ba6364f5d1be8283dc70c9d20dcb56b41a8a87
levysaar <saarlevy@gmail.com>
2019-10-07 22:15:05 +0300

    def_list initial commit

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ module.exports = {
   pluginName: 'SiegeInsightsPlugin',
   pluginDescription: 'Exports siege matchup info and attack logs for Siege Insights (siege.sw-insights.com)',
   data: {},
+  logged_data: { matchup_info: 0, attack_log: 0, defense_log: 0, defense_list: 0 },
+  match_id: undefined,
   init(proxy, config) {
     proxy.on('GetGuildSiegeMatchupInfo', (req, resp) => {
       if (config.Config.Plugins[this.pluginName].enabled) {
         this.data['wizard_id'] = req.wizard_id;
         this.data['matchup_info'] = resp;
-        this.writeSiegeMatchToFile(proxy, this.data, resp.match_info.match_id, 'matchup info');
+        this.logged_data.matchup_info = 1;
+        this.match_id = resp.match_info.match_id;
+        this.writeSiegeMatchToFile(proxy, this.data, 'matchup info');
       }
     });
     proxy.on('GetGuildSiegeBattleLog', (req, resp) => {
@@ -22,17 +26,29 @@ module.exports = {
         let log_msg;
         if (req.log_type === 1) {
           this.data['attack_log'] = resp;
+          this.logged_data.attack_log = 1;
           log_msg = 'attack log';
         } else {
           this.data['defense_log'] = resp;
+          this.logged_data.defense_log = 1;
           log_msg = 'defense log';
         }
-        this.writeSiegeMatchToFile(proxy, this.data, resp.log_list[0].guild_info_list[0].match_id, log_msg);
+        this.match_id = resp.log_list[0].guild_info_list[0].match_id;
+        this.writeSiegeMatchToFile(proxy, this.data, log_msg);
+      }
+    });
+    proxy.on('GetGuildSiegeBaseDefenseUnitList', (req, resp) => {
+      if (config.Config.Plugins[this.pluginName].enabled) {
+        if (req.base_number === 1) {
+          this.data['defense_list'] = resp;
+          this.logged_data.defense_list = 1;
+          this.writeSiegeMatchToFile(proxy, this.data, 'defenses list');
+        }
       }
     });
   },
-  writeSiegeMatchToFile(proxy, data, match_id, log_msg) {
-    const filename = sanitize(`SiegeMatch-${match_id}`).concat('.json');
+  writeSiegeMatchToFile(proxy, data, log_msg) {
+    const filename = sanitize(this.match_id ? `SiegeMatch-${this.match_id}` : 'SiegeDefenseList').concat('.json');
 
     const outFile = fs.createWriteStream(path.join(config.Config.App.filesPath, filename), {
       flags: 'w',
@@ -41,6 +57,16 @@ module.exports = {
 
     outFile.write(JSON.stringify(data, true, 2));
     outFile.end();
-    proxy.log({ type: 'success', source: 'plugin', name: this.pluginName, message: `Saved ${log_msg} data to file ${filename}` });
+    const message = `<div>
+                      Saved ${log_msg} data to file ${filename}.
+
+                      <ul>
+                        <li>Active Matchup: ${this.logged_data.matchup_info ? 'v' : 'x'}</li>
+                        <li>Attack Log: ${this.logged_data.attack_log ? 'v' : 'x'}</li>
+                        <li>Defense Log: ${this.logged_data.defense_log ? 'v' : 'x'}</li>
+                        <li>Defense List: ${this.logged_data.defense_list ? 'v' : 'x'}</li>
+                      </ul>
+                     </div>`;
+    proxy.log({ type: 'success', source: 'plugin', name: this.pluginName, message: message });
   }
 };
